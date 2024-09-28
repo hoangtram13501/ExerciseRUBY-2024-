8d2b85e5bdd2755d1052595e02f7de20f0a4a43c
Hoang Tram <hoangtram13501@gmail.com>
2024-09-28 10:14:27 +0700

    create view User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 10.times do |i|
-  User.create(name: "Tram#{i}", email: "tram#{i}@gmail.com")
+  User.create(name: "Tram#{i}", email: "tram#{i}@gmail.com", phone_number: "090#{i}123#{i}")
 end
 
 10.times do |x|
