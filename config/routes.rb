f37a2ec54952a830b6c49ed3dc22b517a19526bd
Hoang Tram <hoangtram13501@gmail.com>
2024-11-08 15:02:16 +0700

    add jwt into authen

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,17 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index destroy]
+      resources :sessions do
+        collection do
+          post 'login', to: 'sessions#create'
+          post 'login', to: 'sessions#create'
+          delete '/logout', to: 'sessions#destroy'
+        end
+      end
+    end
+  end
 end
